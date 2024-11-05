11ec89f8d57511cd6e0c2bc326da39306c99fbb5
unhabae <158564666+unhabae@users.noreply.github.com>
2024-11-05 14:22:05 +0100

    initialize function for klassen grunnstoff

---
+++ grunnstoff.py
@@ -0,0 +1,13 @@
+class Grunnstoff:
+    def __init__(self, navn, atomnummer, symbol, molarMasse, noytron, periode, gruppe, fase, type, elektronegativitet, atomradius):
+        self.navn = navn
+        self.atomnummer = atomnummer
+        self.symbol = symbol
+        self.molarMasse = molarMasse
+        self.noytron = noytron
+        self.periode = periode
+        self.gruppe = gruppe
+        self.fase = fase
+        self.type = type
+        self.elektronegativitet = elektronegativitet
+        self.atomradius = atomradius
