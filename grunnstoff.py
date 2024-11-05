b84c1e7a3d2bc06fff507d77661a4229b2162140
unhabae <158564666+unhabae@users.noreply.github.com>
2024-11-05 14:49:57 +0100

    spesifiserte datatype i __init__

--- grunnstoff.py
+++ grunnstoff.py
@@ -1,18 +1,21 @@
 class Grunnstoff:
     def __init__(self, navn, atomnummer, symbol, molarMasse, noytron, periode, gruppe, fase, type, elektronegativitet, atomradius):
         self.navn = navn
-        self.atomnummer = atomnummer
+        self.atomnummer = int(atomnummer)
         self.symbol = symbol
-        self.molarMasse = molarMasse
-        self.noytron = noytron
-        self.periode = periode
-        self.gruppe = gruppe
+        self.molarMasse = float(molarMasse)
+        self.noytron = int(noytron)
+        self.periode = int(periode)
+        self.gruppe = int(gruppe)
         self.fase = fase
         self.type = type
-        self.elektronegativitet = elektronegativitet
-        self.atomradius = atomradius
+        self.elektronegativitet = float(elektronegativitet) if elektronegativitet else 0
+        self.atomradius = float(atomradius) if atomradius else 0
 
-    def __str__(self):    #Hvordan grunnstoffet skal se ut som en string
+    def __str__(self): 
         return f"Grunnstoffet {self.navn} har atomnummer {self.atomnummer} og symbol {self.symbol}."
     
+    def forholdMellomENogAR(self):
+        #TO DO
+        pass
     
