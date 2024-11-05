344ae04a169b751392b215ae7b4afd4864d6aac8
unhabae <158564666+unhabae@users.noreply.github.com>
2024-11-05 14:27:14 +0100

    foreløpig enkel string function

--- grunnstoff.py
+++ grunnstoff.py
@@ -11,3 +11,8 @@ class Grunnstoff:
         self.type = type
         self.elektronegativitet = elektronegativitet
         self.atomradius = atomradius
+
+    def __str__(self):    #Hvordan grunnstoffet skal se ut som en string
+        return f"Grunnstoffet {self.navn} har atomnummer {self.atomnummer} og symbol {self.symbol}."
+    
+    
