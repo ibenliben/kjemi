f2ecec572d222cc2fc3bc3271b445e4b7c3332d1
unhabae <158564666+unhabae@users.noreply.github.com>
2024-11-05 15:19:58 +0100

    laget dictionary periodicTable

--- main.py
+++ main.py
@@ -2,18 +2,24 @@ import csv
 from grunnstoff import Grunnstoff
 #import matplotlib.pyplot as plt
 
-filnavn = "PeriodicTable.csv"
+filnavn = "PeriodicTable.csv"                               # csv-filen
+periodicTable = {}                                          # dictionary = {symbol; grunnstoffobjekt}
 
 with open(filnavn, encoding="utf-8-sig") as fil:
-  filinnhold = csv.reader(fil, delimiter=";")
+    filinnhold = csv.reader(fil, delimiter=";")
+    
+    # hoppe over overskrifter
+    overskrifter = next(filinnhold)
+    print(overskrifter)
 
-  overskrifter = next(filinnhold)
-  print(overskrifter)
+    for rad in filinnhold:
+        # lage objekt, gi navn 
+        g = Grunnstoff(*rad)                                #explode list to parameters
+        periodicTable[g.symbol] = g
 
-  for rad in filinnhold:
-    # lage objekt, gi navn 
-    g = Grunnstoff(*rad)    #explode list to parameters
+        print(g)
+        #print(g.navn, g.atomnummer, type(g.elektronegativitet))
 
-    print(g)
-    #print(g.navn, g.atomnummer, type(g.elektronegativitet))
+print(periodicTable.keys())
+
 
