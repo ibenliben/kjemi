d68ce479e04e59815737a029a7553102b6c1eb0f
unhabae <158564666+unhabae@users.noreply.github.com>
2024-11-15 13:11:08 +0100

    #4 Forbedret testen for fra_navn_til_dict

--- kjemi_test.py
+++ kjemi_test.py
@@ -1,5 +1,8 @@
 from kjemi_utils import fra_navn_til_dict
 
-resultat = fra_navn_til_dict("Na2SO4")
+stoff = "Na2SO4"
+expected = {"Na":2, "S":1, "O":4}
+resultat = fra_navn_til_dict(stoff)
 
-print(resultat)
+if resultat != expected:
+    print("Feil resultat for", stoff, "forventet", expected, "Men fikk", resultat)
